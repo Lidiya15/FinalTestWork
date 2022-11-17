e16927d8ffd79abee90a08753c48bbb990f2a218
Lidiya <lidia.pahno@yandex.ru>
2022-11-17 23:38:13 +0300

    Создание массива

---
+++ solution/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+using System;
+using static System.Console;
+
+Clear();
+
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n) {
+    string[] output = new string[CountLessThan(input, n)];
