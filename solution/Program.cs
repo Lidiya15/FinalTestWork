7fc06aef0153a332c6fee476d0e5a8fc30412c7d
Lidiya <lidia.pahno@yandex.ru>
2022-11-18 00:03:30 +0300

    Проверили каждое значение массива на соответсвие условию

--- solution/Program.cs
+++ solution/Program.cs
@@ -14,3 +14,25 @@ WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
 
 string[] FindLessThan(string[] input, int n) {
     string[] output = new string[CountLessThan(input, n)];
+
+for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThan(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
